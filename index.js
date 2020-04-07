c615c690a551f2636b1ff3044adabcfc5f930bb1
Konrad Rudnicki <zeglarz@gmail.com>
2020-04-07 14:35:51 +0200

    some improvements

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var allButtons = document.querySelectorAll("button");
+var allButtons = document.querySelectorAll('button');
 var crash = new Audio('sounds/crash.mp3');
 var kickBass = new Audio('sounds/kick-bass.mp3');
 var snare = new Audio('sounds/snare.mp3');
@@ -8,49 +8,56 @@ var tom3 = new Audio('sounds/tom-3.mp3');
 var tom4 = new Audio('sounds/tom-4.mp3');
 
 function triggerSound(key) {
-  switch (key) {
-  case "w":
-  crash.play();
-  break;
-  case "a":
-  kickBass.play();
-  break;
-  case "s":
-  snare.play();
-  break;
-  case "d":
-  tom1.play();
-  break;
-  case "j":
-  tom2.play();
-  break;
-  case "k":
-  tom3.play();
-  break;
-  case "l":
-  tom4.play();
-  break;
-  default:
-  console.log(this.innerHTML);
-  }
+    switch (key) {
+        case 'w':
+            crash.currentTime = 0;
+            crash.play();
+            break;
+        case 'a':
+            kickBass.currentTime = 0;
+            kickBass.play();
+            break;
+        case 's':
+            snare.currentTime = 0;
+            snare.play();
+            break;
+        case 'd':
+            tom1.currentTime = 0;
+            tom1.play();
+            break;
+        case 'j':
+            tom2.currentTime = 0;
+            tom2.play();
+            break;
+        case 'k':
+            tom3.currentTime = 0;
+            tom3.play();
+            break;
+        case 'l':
+            tom4.currentTime = 0;
+            tom4.play();
+            break;
+        default:
+            console.log(this.innerHTML);
+    }
 }
 
-for (let i=0; i <= document.querySelectorAll(".drum").length; i++) {
-allButtons[i].addEventListener("click", function() {
-triggerSound(this.innerHTML);
-buttonAnimaton(this.innerHTML);
-});
-document.addEventListener("keydown", function(event) {
-triggerSound(event.key);
-buttonAnimaton(event.key);
-});
+for (let i = 0; i <= document.querySelectorAll(".drum").length; i++) {
+    allButtons[i].addEventListener('click', function () {
+        triggerSound(this.innerHTML);
+        buttonAnimaton(this.innerHTML);
+    });
+    document.addEventListener('keydown', function (event) {
+        triggerSound(event.key);
+        buttonAnimaton(event.key);
+    });
 }
 
 function buttonAnimaton(currentKey) {
-  var activeButton = document.querySelector("." + currentKey);
-  activeButton.classList.add("pressed");
+    var activeButton = document.querySelector('.' + currentKey);
+    activeButton.classList.add('pressed');
 
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 100);
+    setTimeout(function () {
+        activeButton.classList.remove('pressed');
+    }, 100);
 }
